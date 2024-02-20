78bdefc6f04cdb495a315a564bec523beec1a54f
陳冠宇 <chen630602@gmail.com>
2024-02-20 17:00:01 +0800

    2024/02/20
    作業3

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-const app = Vue.creatapp({
-  data() {},
-  watch() {},
-  computed() {},
-  methods() {},
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  watch: {
+    result() {
+      // 監聽 computed 的 result
+      console.log("watch test");
+      const that = this;
+      setTimeout(function () {
+        that.counter = 0;
+      }, 5000); // 5秒後歸零
+    },
+  },
+  computed: {
+    result() {
+      if (this.counter < 37) {
+        return "Not there yet";
+      } else if (this.counter === 37) {
+        return this.counter;
+      } else {
+        return "Too much!";
+      }
+    },
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+  },
 });
+
+app.mount("#assignment");
